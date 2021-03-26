755036f1adc43c6e05e82905614e222fe22e4723
kwangyel <kwangyel@gmail.com>
2021-03-25 23:36:09 -0700

    api token based authentication

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,13 @@ Route::get('/' , function(){
     return ['message' => 'Welcome to UN BHUTAN e-Pass API'];
 });
 
+Route::get('/protected' , function(){
+    return ['message' => 'this is a protected message'];
+})->middleware('auth:api');
+
+Route::post('/create','AuthController@create');
+Route::post('/login','AuthController@login');
+
 Route::resource('/agency' , 'AgencyController');
 
 Route::resource('/car' , 'CarController');
@@ -27,7 +34,6 @@ Route::resource('/staff' , 'StaffController');
 Route::resource('/visitor' , 'VisitorController');
 
 Route::resource('/check' , 'CheckController');
-
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
